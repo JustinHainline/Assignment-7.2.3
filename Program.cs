c8723b87ab7df8ac25dc878fdc240c14003fb327
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-20 08:15:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment_7._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = IsAnagram("anagram", "anagram");
+            Console.WriteLine($"Is 'anagram' an anagram of 'anagram'? {result}");
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            int[] charCount = new int[26]; // Assuming lowercase English letters
+
+            // Count characters in s
+            foreach (char c in s)
+                charCount[c - 'a']++;
+
+            // Subtract characters in t
+            foreach (char c in t)
+            {
+                charCount[c - 'a']--;
+                if (charCount[c - 'a'] < 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
